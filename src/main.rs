f3275f93b08c60ddf93de794717ce644d69999f2
Mitchell Luhm <sharperacc@gmail.com>
2020-02-04 22:44:21 -0600

    can now extract uploader from filenames

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,52 @@ fn main()
     
 fn run(path: String)
 {
+    let file_names: Vec<String> = get_file_names(path);
+    println!("{:?}", file_names);
+    for file_name in file_names.iter()
+    {
+        let mut underscore_count: u8 = 0;
+        let mut us_pos: Vec<usize> = Vec::with_capacity(2);
+
+        for c in file_name.char_indices().rev()
+        {
+            if c.1 == '_'
+            {
+                underscore_count += 1;
+                us_pos.push(c.0);
+            }
+
+            if underscore_count == 2
+            {
+                if let Some(uploader) = file_name.get(us_pos[1] + 1..us_pos[0])
+                {
+                    println!("{}", uploader);
+                }
+                break;
+            }
+            //match c
+            //{
+                // (index, '_') => underscore_count += 1; underscore_indices.push(index)),
+                // _ => (),
+            // }
+        }
+    }
+}
+
+fn get_file_names(path: String) -> Vec<String>
+{
+    let mut file_names: Vec<String> = Vec::new();
     if let Ok(entries) = fs::read_dir(&path)
     {
         for entry in entries
         {
             if let Ok(entry) = entry
             {
-                println!("{:?}", entry.file_name());
+                match entry.file_name().into_string()
+                {
+                    Ok(file_name) => file_names.push(file_name),
+                    Err(_) => println!("Error converting OsString to String."),
+                }
             }
         }
     }
@@ -31,4 +70,5 @@ fn run(path: String)
         println!("The path: {} could not be read.", path)
     }
 
+    file_names
 }
